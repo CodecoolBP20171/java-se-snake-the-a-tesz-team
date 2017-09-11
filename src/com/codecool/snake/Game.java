be11773bdba91da4ecf8e0683e73331126455848
barnabastoth <barnabastoth94@gmail.com>
2017-09-11 16:31:01 +0200

    Game.java now generates 5 powerups randomly at start

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -8,35 +8,45 @@ import com.codecool.snake.entities.powerups.TurnRatePowerUp;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import sun.java2d.pipe.SpanShapeRenderer;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Game extends Pane {
+
+    public static int numOfPowerUps;
 
     public Game() {
         new SnakeHead(this, 500, 500);
 
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-
-        new SimplePowerup(this);
-        new SimplePowerup(this);
-        new SimplePowerup(this);
-        new SimplePowerup(this);
-        new SlowMotionPowerUp(this);
-        new SlowMotionPowerUp(this);
-
-        new TurnRatePowerUp(this);
-        new TurnRatePowerUp(this);
-        new TurnRatePowerUp(this);
-        new TurnRatePowerUp(this);
-
-        new SpeedingPowerUp(this);
-        new SpeedingPowerUp(this);new SpeedingPowerUp(this);new SpeedingPowerUp(this);new SpeedingPowerUp(this);new SpeedingPowerUp(this);
-
+        generatePowerUp(this);
 
 
     }
+
+    public void generatePowerUp(Game game) {
+        while (numOfPowerUps <= 5) {
+            Random rnd = new Random();
+            int powerUpRandom = rnd.nextInt(4 - 1) + 1;
+            if (powerUpRandom == 1) {
+                new SlowMotionPowerUp(game);
+            } else if (powerUpRandom == 2) {
+                new SimplePowerup(game);
+            } else if(powerUpRandom == 3) {
+                new SpeedingPowerUp(game);
+            } else if (powerUpRandom == 4) {
+                new TurnRatePowerUp(game);
+            }
+            numOfPowerUps++;
+        }
+    }
+
+
 
     public void start() {
         Scene scene = getScene();
