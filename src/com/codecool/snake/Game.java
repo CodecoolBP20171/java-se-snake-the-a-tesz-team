b6a831c02b7cb0259cc13fd377b978e5d5bc67b5
Unknown <paalginaa@gmail.com>
2017-09-12 10:40:27 +0200

    start method: case SPACE return value modification

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -31,7 +31,7 @@ public class Game extends Pane {
             switch (event.getCode()) {
                 case LEFT:  Globals.leftKeyDown  = true; break;
                 case RIGHT: Globals.rightKeyDown  = true; break;
-                case SPACE: Globals.spaceDown = true; break;
+                case SPACE: Globals.spaceDown = false; break;
             }
         });
 
@@ -39,7 +39,7 @@ public class Game extends Pane {
             switch (event.getCode()) {
                 case LEFT:  Globals.leftKeyDown  = false; break;
                 case RIGHT: Globals.rightKeyDown  = false; break;
-                case SPACE: Globals.spaceDown = false; break;
+                case SPACE: Globals.spaceDown = true; break;
             }
         });
         Globals.gameLoop = new GameLoop();
